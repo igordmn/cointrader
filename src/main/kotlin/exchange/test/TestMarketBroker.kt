05c7cabeebdb58d0719b2d95df5b808cc20d7c99
Igor Demin <igordmn@gmail.com>
2018-01-16 11:03:07 +0300

    -

--- src/main/kotlin/exchange/test/TestMarketBroker.kt
+++ src/main/kotlin/exchange/test/TestMarketBroker.kt
@@ -16,37 +16,53 @@ class TestMarketBroker(
 ) : MarketBroker {
     override suspend fun buy(amount: BigDecimal) {
         val currentPrice = price.current()
+        val limits = limits.get()
+        val roundedAmount = if (limits.amountStep > BigDecimal.ZERO) {
+            amount.divide(limits.amountStep, 0, RoundingMode.FLOOR) * limits.amountStep
+        } else {
+            amount
+        }
+        if (roundedAmount * currentPrice >= limits.minTotalPrice) {
+            buyToPortfolio(roundedAmount, currentPrice)
+        }
+    }
+
+    private suspend fun buyToPortfolio(amount: BigDecimal, currentPrice: BigDecimal) {
         portfolio.modify {
-            val limits = limits.get()
-            val roundedAmount = amount.divide(limits.amountStep, 0, RoundingMode.FLOOR) * limits.amountStep
-            if (roundedAmount >= limits.minTotalPrice) {
-                val fromAmount = it[fromCoin]
-                val fromSellAmount = amount * currentPrice
-                if (fromSellAmount <= fromAmount) {
-                    it[fromCoin] = it[fromCoin] - fromSellAmount
-                    it[toCoin] = it[toCoin] + amount * (BigDecimal.ONE - fee)
-                } else {
-                    it[fromCoin] = BigDecimal.ZERO
-                    it[toCoin] = it[toCoin] + fromAmount / currentPrice * (BigDecimal.ONE - fee)
-                }
+            val fromAmount = it[fromCoin]
+            val fromSellAmount = amount * currentPrice
+            if (fromSellAmount <= fromAmount) {
+                it[fromCoin] = it[fromCoin] - fromSellAmount
+                it[toCoin] = it[toCoin] + amount * (BigDecimal.ONE - fee)
+            } else {
+                it[fromCoin] = BigDecimal.ZERO
+                it[toCoin] = it[toCoin] + fromAmount / currentPrice * (BigDecimal.ONE - fee)
             }
         }
     }
 
     override suspend fun sell(amount: BigDecimal) {
         val currentPrice = price.current()
+        val limits = limits.get()
+        val roundedAmount = if (limits.amountStep > BigDecimal.ZERO) {
+            amount.divide(limits.amountStep, 0, RoundingMode.FLOOR) * limits.amountStep
+        } else {
+            amount
+        }
+        if (roundedAmount * currentPrice >= limits.minTotalPrice) {
+            sellFromPortfolio(roundedAmount, currentPrice)
+        }
+    }
+
+    private suspend fun sellFromPortfolio(amount: BigDecimal, currentPrice: BigDecimal) {
         portfolio.modify {
-            val limits = limits.get()
-            val roundedAmount = amount.divide(limits.amountStep, 0, RoundingMode.FLOOR) * limits.amountStep
-            if (roundedAmount >= limits.minTotalPrice) {
-                val toAmount = it[toCoin]
-                if (amount <= toAmount) {
-                    it[fromCoin] = it[fromCoin] + amount * currentPrice * (BigDecimal.ONE - fee)
-                    it[toCoin] = it[toCoin] - amount
-                } else {
-                    it[fromCoin] = it[fromCoin] + toAmount * currentPrice * (BigDecimal.ONE - fee)
-                    it[toCoin] = BigDecimal.ZERO
-                }
+            val toAmount = it[toCoin]
+            if (amount <= toAmount) {
+                it[fromCoin] = it[fromCoin] + amount * currentPrice * (BigDecimal.ONE - fee)
+                it[toCoin] = it[toCoin] - amount
+            } else {
+                it[fromCoin] = it[fromCoin] + toAmount * currentPrice * (BigDecimal.ONE - fee)
+                it[toCoin] = BigDecimal.ZERO
             }
         }
     }
--- src/test/kotlin/exchange/test/TestMarketBrokerSpec.kt
+++ src/test/kotlin/exchange/test/TestMarketBrokerSpec.kt
@@ -1,70 +1,162 @@
 package exchange.test
 
 import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.FreeSpec
 import io.kotlintest.specs.StringSpec
 import kotlinx.coroutines.experimental.runBlocking
 import java.math.BigDecimal
+import java.math.RoundingMode
 
-class TestMarketBrokerSpec : StringSpec({
+class TestMarketBrokerSpec : FreeSpec({
     val ltcInBtcPrice = TestMarketPrice(BigDecimal("0.01"))
     val ethInBtcPrice = TestMarketPrice(BigDecimal("0.1"))
-    val portfolio = TestPortfolio(mapOf(
-            "BTC" to BigDecimal("1.0"),
-            "LTC" to BigDecimal("20.0"),
-            "ETH" to BigDecimal("25.0")
-    ))
-    // todo fee
-    val btcToLtc = TestMarketBroker("BTC", "LTC", portfolio, ltcInBtcPrice, BigDecimal.ZERO, TestMarketLimits(BigDecimal.ZERO, BigDecimal.ZERO))
-    val btcToEth = TestMarketBroker("BTC", "ETH", portfolio, ethInBtcPrice, BigDecimal.ZERO, TestMarketLimits(BigDecimal.ZERO, BigDecimal.ZERO))
 
-    "buy LTC" {
-        runBlocking {
-            btcToLtc.buy(BigDecimal(10.0))
-            val amounts = portfolio.amounts()
-            amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("0.90")
-            amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("30.00")
-            amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("25.00")
+    "zero fee, zero limits" - {
+        val portfolio = TestPortfolio(mapOf(
+                "BTC" to BigDecimal("1.0"),
+                "LTC" to BigDecimal("20.0"),
+                "ETH" to BigDecimal("25.0")
+        ))
+        val btcToLtc = TestMarketBroker("BTC", "LTC", portfolio, ltcInBtcPrice, BigDecimal.ZERO, TestMarketLimits(BigDecimal.ZERO, BigDecimal.ZERO))
+        val btcToEth = TestMarketBroker("BTC", "ETH", portfolio, ethInBtcPrice, BigDecimal.ZERO, TestMarketLimits(BigDecimal.ZERO, BigDecimal.ZERO))
+
+        "buy LTC" {
+            runBlocking {
+                btcToLtc.buy(BigDecimal(10.0))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("0.90")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("30.00")
+                amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("25.00")
+            }
+        }
+
+        "sell LTC" {
+            runBlocking {
+                btcToLtc.sell(BigDecimal(5.0))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("1.05")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("15.00")
+                amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("25.00")
+            }
+        }
+
+        "sell LTC, buy ETH" {
+            runBlocking {
+                btcToLtc.sell(BigDecimal(10.0))
+                btcToEth.buy(BigDecimal(5.0))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("0.60")  // 1.0 + 10 * 0.01 - 0.1 * 5
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("10.00")
+                amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("30.00")
+            }
+        }
+
+        "sell LTC greater than can sell" {
+            runBlocking {
+                btcToLtc.sell(BigDecimal(1000.0))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("1.20")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("0.00")
+                amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("25.00")
+            }
+        }
+
+        "buy LTC greater than can buy" {
+            runBlocking {
+                btcToLtc.buy(BigDecimal(1000.0))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("0.00")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("120.00")
+                amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("25.00")
+            }
         }
     }
 
-    "sell LTC" {
-        runBlocking {
-            btcToLtc.sell(BigDecimal(5.0))
-            val amounts = portfolio.amounts()
-            amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("1.05")
-            amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("15.00")
-            amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("25.00")
+    "0.1 fee, zero limits" - {
+        val portfolio = TestPortfolio(mapOf(
+                "BTC" to BigDecimal("1.0"),
+                "LTC" to BigDecimal("20.0")
+        ))
+        val btcToLtc = TestMarketBroker("BTC", "LTC", portfolio, ltcInBtcPrice, BigDecimal("0.1"), TestMarketLimits(BigDecimal.ZERO, BigDecimal.ZERO))
+
+        "buy LTC" {
+            runBlocking {
+                btcToLtc.buy(BigDecimal(10.0))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("0.90")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("29.00")
+            }
+        }
+
+        "sell LTC" {
+            runBlocking {
+                btcToLtc.sell(BigDecimal(5.0))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(3) shouldBe BigDecimal("1.045")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("15.00")
+            }
         }
     }
 
-    "sell LTC, buy ETH" {
-        runBlocking {
-            btcToLtc.sell(BigDecimal(10.0))
-            btcToEth.buy(BigDecimal(5.0))
-            val amounts = portfolio.amounts()
-            amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("0.60")  // 1.0 + 10 * 0.01 - 0.1 * 5
-            amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("10.00")
-            amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("30.00")
-        }
-    }
+    "zero fee, with limits" - {
+        val portfolio = TestPortfolio(mapOf(
+                "BTC" to BigDecimal("1.0"),
+                "LTC" to BigDecimal("20.0")
+        ))
+        val btcToLtc = TestMarketBroker("BTC", "LTC", portfolio, ltcInBtcPrice, BigDecimal.ZERO, TestMarketLimits(BigDecimal("0.1"), BigDecimal("0.01")))
 
-    "sell LTC greater than can sell" {
-        runBlocking {
-            btcToLtc.sell(BigDecimal(1000.0))
-            val amounts = portfolio.amounts()
-            amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("1.20")
-            amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("0.00")
-            amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("25.00")
+        "not buying any LTC if less than limit" {
+            runBlocking {
+                btcToLtc.buy(BigDecimal(0.09))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("1.00")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("20.00")
+            }
         }
-    }
 
-    "buy LTC greater than can buy" {
-        runBlocking {
-            btcToLtc.buy(BigDecimal(1000.0))
-            val amounts = portfolio.amounts()
-            amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("0.00")
-            amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("120.00")
-            amounts["ETH"]!!.setScale(2) shouldBe BigDecimal("25.00")
+        "not selling any LTC if less than limit" {
+            runBlocking {
+                btcToLtc.sell(BigDecimal(0.99))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("1.00")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("20.00")
+            }
+        }
+
+        "not buying any LTC if less than total price limit" {
+            runBlocking {
+                btcToLtc.buy(BigDecimal(0.99))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("1.00")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("20.00")
+            }
+        }
+
+        "not selling any LTC if less than total price limit" {
+            runBlocking {
+                btcToLtc.sell(BigDecimal(0.09))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("1.00")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("20.00")
+            }
+        }
+
+        "buying rounded LTC if greater than limit" {
+            runBlocking {
+                btcToLtc.buy(BigDecimal(1.09))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("0.99")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("21.00")
+            }
+        }
+
+        "selling rounded LTC if greater than limit" {
+            runBlocking {
+                btcToLtc.sell(BigDecimal(1.09))
+                val amounts = portfolio.amounts()
+                amounts["BTC"]!!.setScale(2) shouldBe BigDecimal("1.01")
+                amounts["LTC"]!!.setScale(2) shouldBe BigDecimal("19.00")
+            }
         }
     }
 })
