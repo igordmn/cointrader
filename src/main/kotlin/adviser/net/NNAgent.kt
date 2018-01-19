d55a88230bb9a1f3df1666c0df55dd39b590a015
Igor Demin <igordmn@gmail.com>
2018-01-19 22:18:31 +0300

    -

--- src/main/kotlin/adviser/net/NNAgent.kt
+++ src/main/kotlin/adviser/net/NNAgent.kt
@@ -22,26 +22,13 @@ class NNAgent(
         require(history.n3 == altCoinNumber)
         require(history.n4 == windowSize)
 
-        println("00000000000000000000``")
-
-        val callMethod = numpy.callMethod("array", history.data)
-        println("111111111111111111111``")
-        val nphistory = callMethod
+        val nphistory = numpy.callMethod("array", history.data)
                 .callMethod("reshape", intArrayOf(history.n1, history.n2, history.n3, history.n4))
-        println("22222222222222222222222``")
-        val callMethod1 = numpy.callMethod("array", currentPortions.data)
-        println("333333333333333333333333``")
-        val npportions = callMethod1
+        val npportions = numpy.callMethod("array", currentPortions.data)
                 .callMethod("reshape", intArrayOf(currentPortions.n1, currentPortions.n2))
-        println("444444444444444444444444444444444``")
-        val callMethod2 = agent.callMethod("best_portfolio", nphistory, npportions)
-        println("555555555555555555555555555555555``")
-        val callMethod3 = callMethod2.callMethod("flatten")
-        println("5555555555555555555555555555555555555555``")
-        val result = callMethod3.callMethod("tolist")
-        println("66666666666666666666666666666666``")
+
+        val result = agent.callMethod("best_portfolio", nphistory, npportions).callMethod("flatten").callMethod("tolist")
         val data = PythonUtils.getDoubleArrayValue(result)
-        println("77777777777777777777777777777777777777777``")
 
         return DoubleMatrix2D(history.n1, altCoinNumber + 1, data)
     }
