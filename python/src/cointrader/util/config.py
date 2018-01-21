a418854d6a5eecb1019a0676b7e9219379f0d999
Igor Demin <igordmn@gmail.com>
2018-01-21 21:51:13 +0300

    -

--- python/src/cointrader/util/config.py
+++ python/src/cointrader/util/config.py
@@ -17,7 +17,7 @@ class TrainConfig(NamedTuple):
     window_size: int = 160
     batch_size: int = 109
     coin_number: int = 39
-    steps: int = 40000
+    steps: int = 60000
     log_steps: int = 1000
     period: int = 300
     start_time: int = parse_time("2017/8/1 00:00:00")
