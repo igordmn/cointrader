628b5d4989610702a7df94e30139808b9168a971
Igor Demin <igordmn@gmail.com>
2018-01-24 09:37:51 +0300

    -

--- python/src/cointrader/util/backtest.py
+++ python/src/cointrader/util/backtest.py
@@ -4,40 +4,35 @@ from src.cointrader.util.indicators import compute_max_drawdown, sharpe, positiv
 
 
 def backtest(setname, agent, matrix, config, log):
-    x, price_inc, prices, w = matrix.get_validation_set() if setname == "validation" else matrix.get_test_set()
+    x, price_inc, all_prices, w = matrix.get_validation_set() if setname == "validation" else matrix.get_test_set()
     total_steps = x.shape[0]
 
     def normalize_portfolio(portfolio):
         return portfolio / np.sum(portfolio)
 
+    def rebalance(portfolio, new_portfolio_percents):
+        capital = np.sum(portfolio)
+        desired_portfolio = capital * new_portfolio_percents
+        total_fee = np.sum(np.abs(desired_portfolio - portfolio) * config.fee)
+        capital_after_fee = capital - total_fee
+        return capital_after_fee * new_portfolio_percents
+
     def trade_single(step, portfolio):
         history = x[step][np.newaxis, :, :, :]
 
-        train_prices = np.concatenate((np.ones(1), prices[step]))
+        prices = np.concatenate((np.ones(1), all_prices[step]))
 
-        portfolio_percents = normalize_portfolio(portfolio)
+        portfolio_btc = portfolio * prices
+        portfolio_percents = normalize_portfolio(portfolio_btc)
         result = np.squeeze(agent.best_portfolio(history, portfolio_percents[np.newaxis, 1:]))
         new_portfolio_percents = normalize_portfolio(result)
-        log("suggestion", ", ".join("%.2f" % f for f in new_portfolio_percents))
-        current_index = portfolio_percents.tolist().index(max(portfolio_percents))
-        buy_index = new_portfolio_percents.tolist().index(max(new_portfolio_percents))
+        log("portfolio", ", ".join("%.2f" % f for f in new_portfolio_percents))
 
-        if buy_index != current_index:
-            current_price = train_prices[current_index]
-            buy_price = train_prices[buy_index]
-
-            if current_index != 0:
-                old_amount = portfolio[current_index]
-                portfolio[current_index] = 0.0
-                portfolio[0] = old_amount * current_price * (1 - config.fee)
-
-            if buy_index != 0 and portfolio[0] > 0:
-                old_amount = portfolio[0]
-                portfolio[0] = 0.0
-                portfolio[buy_index] = old_amount / buy_price * (1 - config.fee)
+        portfolio_btc = rebalance(portfolio_btc, new_portfolio_percents)
+        return portfolio_btc / prices
 
     def compute_capital(step, portfolio):
-        current_prices = np.concatenate((np.ones(1), prices[step]))
+        current_prices = np.concatenate((np.ones(1), all_prices[step]))
         sum = 0.0
         for i in range(0, len(portfolio)):
             sum += portfolio[i] * current_prices[i]
@@ -50,19 +45,13 @@ def backtest(setname, agent, matrix, config, log):
 
         for step in range(0, total_steps - 1):
             capital = compute_capital(step, portfolio)
-            # log("=" * 30)
-            # log("the step is {}".format(step))
-            trade_single(step, portfolio)
-            # log("portfolio is " + ", ".join("%.2f" % f for f in portfolio))
-            # log('capital is %3f BTC' % capital)
+            portfolio = trade_single(step, portfolio)
             capitals.append(capital)
 
         periods_per_day = int(24 * 60 * 60 / config.period)
         total_periods = total_steps - 1
         total_profit = compute_capital(total_periods - 1, portfolio)
         one_day_profit = total_profit ** (periods_per_day / total_periods)
-        ten_days_profit = total_profit ** (10 * periods_per_day / total_periods)
-        # log("10 days profit", ten_days_profit)
         log("1 day profit", one_day_profit)
 
         profits = np.array([nxt / current for current, nxt in zip(capitals, capitals[1:])])
@@ -75,4 +64,3 @@ def backtest(setname, agent, matrix, config, log):
         return one_day_profit, capitals
 
     return trade_all()
-
