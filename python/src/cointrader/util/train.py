a51a42839b3c5bc90418040db2e70154dc1d6c3f
Igor Demin <igordmn@gmail.com>
2018-01-24 17:51:22 +0300

    -

--- python/src/cointrader/util/train.py
+++ python/src/cointrader/util/train.py
@@ -24,7 +24,7 @@ def train_net(agent, matrix, config, log):
             log('1 day profit (train)', (total_train_profit ** (1 / config.log_steps)) ** periods_per_day)
             log('1 day profit', geometric_mean_profit ** periods_per_day)
             log('standard_deviation', standard_profit_deviation)
-            log('downside_profit_deviation', downside_profit_deviation)
+            log('downside_deviation', downside_profit_deviation)
             log('sharp_ratio', sharp_ratio)
             log('sortino_ratio', sortino_ratio)
             # print('10 days profit', geometric_mean_profit ** (10 * periods_per_day))
