c1abd5f4f3c6713ef09e94f164db63b8f568ed9e
Igor Demin <igordmn@gmail.com>
2018-01-27 16:52:20 +0300

    -

--- app/src/main/kotlin/main/analyze/date20180127/slippage/AnalyzeSlippage.kt
+++ app/src/main/kotlin/main/analyze/date20180127/slippage/AnalyzeSlippage.kt
@@ -1,21 +1,36 @@
 package main.analyze.date20180127.slippage
 
+import util.math.sum
 import java.math.BigDecimal
+import java.math.RoundingMode
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 fun main(args: Array<String>) {
-    val lines = realTradeLines()
+    val tradeBuilder = TradeBuilder()
+
+    val trades = realTradeLines()
             .map(::parseLine)
-            .filter { it != Line.Unknown }.take(10).toList()
-    print(lines)
+            .filter { it != Line.Unknown }
+            .map(tradeBuilder::buildNext)
+            .filterNotNull()
+
+
+    val comissions = trades
+            .map { it.capitalAfter.divide(it.capitalBefore, 30, RoundingMode.HALF_UP) }
+            .toList()
+
+    val totalCommission = comissions.reduce { acc, item -> acc * item }
+    val meanCommission = Math.pow(totalCommission.toDouble(), 1.0 / comissions.size)
+
+    println(meanCommission)
 }
 
 private fun parseLine(line: String): Line = when {
     line.endsWith("trader.AdvisableTrade real afterGetCapitals") -> Line.AfterGetCapitals(parseTime(line))
-    line.contains("trader.AdvisableTrade real AfterTrade") -> Line.AfterTrade(parseTime(line))
+    line.contains("trader.AdvisableTrade real afterTrade") -> Line.AfterTrade(parseTime(line))
     line.contains("e.binance.market.BinanceMarketBroker Sell") -> parseSell(line)
     line.contains("e.binance.market.BinanceMarketBroker Buy") -> parseBuy(line)
     line.startsWith("capitals {") -> parseCapitals(line)
@@ -26,7 +41,7 @@ private fun parseTime(line: String): Instant {
     val words = line.split(" ")
     val timeStr = words[0] + " " + words[1]
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-    return LocalDateTime.parse(timeStr, formatter).toInstant(ZoneOffset.of("Europe/Moscow"))
+    return LocalDateTime.parse(timeStr, formatter).toInstant(ZoneOffset.of("+3"))
 }
 
 private fun parseSell(line: String): Line {
@@ -59,4 +74,60 @@ sealed class Line {
     data class Buy(val time: Instant, val coin: String) : Line()
     data class Capitals(val map: Map<String, BigDecimal>) : Line()
     object Unknown : Line()
+}
+
+data class Trade(val time: Instant, val fromCoin: String, val toCoin: String, val capitalBefore: BigDecimal, val capitalAfter: BigDecimal)
+
+class TradeBuilder {
+    private var state: State = State.BeforeTrade
+
+    fun buildNext(line: Line): Trade? {
+        val state = state
+
+        when (line) {
+            is Line.AfterGetCapitals -> {
+                this.state = State.AfterGetCapitals(line.time)
+            }
+            is Line.Capitals -> when (state) {
+                is State.AfterGetCapitals -> {
+                    this.state = State.CapitalsBeforeTrade(state.time, capital(line.map))
+                }
+                is State.AfterTrade -> {
+                    val result = Trade(state.time, state.fromCoin, state.toCoin, state.capitalBefore, capital(line.map))
+                    this.state = State.BeforeTrade
+                    return result
+                }
+            }
+            is Line.Sell -> if (state is State.CapitalsBeforeTrade) {
+                this.state = State.Sell(state.time, state.capitalBefore, line.coin)
+            } else {
+                this.state = State.BeforeTrade
+            }
+
+            is Line.Buy -> if (state is State.Sell) {
+                this.state = State.Buy(state.time, state.capitalBefore, state.fromCoin, line.coin)
+            } else {
+                this.state = State.BeforeTrade
+            }
+
+            is Line.AfterTrade -> if (state is State.Buy) {
+                this.state = State.AfterTrade(state.time, state.capitalBefore, state.fromCoin, state.toCoin)
+            } else {
+                this.state = State.BeforeTrade
+            }
+        }
+
+        return null
+    }
+
+    private fun capital(capitals: Map<String, BigDecimal>) = capitals.values.sum()
+
+    private sealed class State {
+        object BeforeTrade : State()
+        data class AfterGetCapitals(val time: Instant) : State()
+        data class CapitalsBeforeTrade(val time: Instant, val capitalBefore: BigDecimal) : State()
+        data class Sell(val time: Instant, val capitalBefore: BigDecimal, val fromCoin: String) : State()
+        data class Buy(val time: Instant, val capitalBefore: BigDecimal, val fromCoin: String, val toCoin: String) : State()
+        data class AfterTrade(val time: Instant, val capitalBefore: BigDecimal, val fromCoin: String, val toCoin: String) : State()
+    }
 }
