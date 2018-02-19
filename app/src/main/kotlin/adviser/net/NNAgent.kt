cbe3cb4d30bc969218cde636321e807d3f3fa306
Igor Demin <igordmn@gmail.com>
2018-02-19 17:37:29 +0300

    -

--- app/src/main/kotlin/adviser/net/NNAgent.kt
+++ app/src/main/kotlin/adviser/net/NNAgent.kt
@@ -5,6 +5,7 @@ import jep.NDArray
 import util.math.DoubleMatrix2D
 import util.math.DoubleMatrix4D
 import java.math.BigDecimal
+import java.nio.file.Path
 
 class NNAgent(
         private val jep: Jep,
@@ -14,7 +15,7 @@ class NNAgent(
         private val fee: BigDecimal,
         private val learningRate: BigDecimal,
         netPath: String? = null
-) {
+): AutoCloseable {
     init {
         jep.eval("from cointrader.util.nnagent import NNAgent")
         jep.eval("from cointrader.util.nnagent import NNConfig")
@@ -38,6 +39,7 @@ class NNAgent(
         require(history.n2 == indicatorNumber)
         require(history.n3 == coinNumber)
         require(history.n4 == windowSize)
+        require(currentPortions.n1 == history.n1)
 
         val nphistory = NDArray(history.data, history.n1, history.n2, history.n3, history.n4)
         val npportions = NDArray(currentPortions.data, currentPortions.n1, currentPortions.n2)
@@ -47,4 +49,41 @@ class NNAgent(
         val dataDouble = result.data.map { it.toDouble() }.toDoubleArray()
         return DoubleMatrix2D(result.dimensions[0], result.dimensions[1], dataDouble)
     }
+
+    @Suppress("UNCHECKED_CAST")
+    fun train(currentPortions: DoubleMatrix2D, history: DoubleMatrix4D, priceIncs: DoubleMatrix2D): TrainResult = synchronized(this) {
+        require(currentPortions.n2 == coinNumber)
+        require(history.n2 == indicatorNumber)
+        require(history.n3 == coinNumber)
+        require(history.n4 == windowSize)
+        require(priceIncs.n2 == coinNumber)
+        require(priceIncs.n1 == currentPortions.n1)
+        require(priceIncs.n1 == history.n1)
+
+        val nphistory = NDArray(history.data, history.n1, history.n2, history.n3, history.n4)
+        val npportions = NDArray(currentPortions.data, currentPortions.n1, currentPortions.n2)
+        val npPriceIncs = NDArray(priceIncs.data, priceIncs.n1, priceIncs.n2)
+
+        val result = jep.invoke("train", nphistory, npportions, npPriceIncs) as Array<*>
+        val newPortions = result[0] as NDArray<FloatArray>
+        val geometricMeanProfit = result[1] as Double
+
+        val portionsDataDouble = newPortions.data.map { it.toDouble() }.toDoubleArray()
+
+        return TrainResult(
+                DoubleMatrix2D(newPortions.dimensions[0], newPortions.dimensions[1], portionsDataDouble),
+                geometricMeanProfit
+        )
+    }
+
+    fun save(path: Path) {
+        val pathStr = path.toAbsolutePath().toString()
+        jep.eval("agent.save($pathStr)")
+    }
+
+    override fun close() {
+        jep.eval("agent.recycle()")
+    }
+
+    data class TrainResult(val newPortions: DoubleMatrix2D, val geometricMeanProfit: Double)
 }
