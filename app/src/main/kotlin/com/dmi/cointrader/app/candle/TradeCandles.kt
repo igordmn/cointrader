e49f8361a79752af721a0eec83ba295a9064e2a7
Igor Demin <igordmn@gmail.com>
2018-03-03 15:51:52 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/app/candle/TradeCandles.kt
+++ app/src/main/kotlin/com/dmi/cointrader/app/candle/TradeCandles.kt
@@ -15,17 +15,6 @@ fun periodNum(startTime: Instant, period: Duration, time: Instant): Long {
     return Duration.between(time, startTime).toMillis() / period.toMillis()
 }
 
-fun timeRangeSequence(startTime: Instant, endTime: Instant, period: Duration): Sequence<InstantRange> {
-    return buildSequence {
-        var start = startTime
-        while (start < endTime) {
-            val end = min(endTime, start + period)
-            yield(start..end)
-            start = end
-        }
-    }
-}
-
 fun <INDEX> ReceiveChannel<IndexedTrade<INDEX>>.candles(
         startTime: Instant,
         period: Duration,
@@ -34,108 +23,16 @@ fun <INDEX> ReceiveChannel<IndexedTrade<INDEX>>.candles(
     fun candlesWithTrades(): ReceiveChannel<TradesCandle<INDEX>> = produce<TradesCandle<INDEX>> {
         var trades = ArrayList<IndexedTrade<INDEX>>()
 
+        val it = iterator()
 
-        for (range in timeRangeSequence(startTime, endTime, period)) {
+        var trade = it.next()
 
-        }
+        for (num in numRange) {
+            val startTime = ;
+            val endTime = ;
 
 
-        val candleBuilder = CandleBuilder(startTime, period)
-        takeWhile { it.value.time < endTime }.consumeEach {
-            val candle = candleBuilder.addAndBuild(it)
-            if (candle != null) {
-                send(candle)
-            }
-        }
-        val lastCandle = candleBuilder.buildLast()
-        if (lastCandle != null) {
-            send(lastCandle)
-        }
-    }
-}
 
-class CandleBuilder(private val startTime: Instant, private val period: Duration) {
-    private val trades = ArrayList<TradeItem>()
-    private var periodIndex: Long = -1
-
-    fun addAndBuild(trade: TradeItem): CandleItem? {
-        val periodIndex = periodNum(startTime, period, trade.value.time)
-        return if (trades.isNotEmpty()) {
-            require(periodIndex >= this.periodIndex)
-            if (periodIndex == this.periodIndex) {
-                trades.add(trade)
-                null
-            } else {
-                val candle = build()
-                trades.clear()
-                trades.add(trade)
-                this.periodIndex = periodIndex
-                candle
-            }
-        } else {
-            trades.add(trade)
-            this.periodIndex = periodIndex
-            null
-        }
-    }
-
-    fun buildLast(): CandleItem? = if (trades.isNotEmpty()) build() else null
-
-    private fun build() = CandleItem(
-            NumIdIndex(
-                    periodIndex.apply { require(this >= 0) },
-                    CandleId(trades.first().index.id)
-            ),
-            Candle(
-                    trades.last().value.price,
-                    trades.maxBy { it.value.price }!!.value.price,
-                    trades.minBy { it.value.price }!!.value.price
-            )
-    )
-}
-
-private fun ReceiveChannel<TradeItem>.candlesWithTrades(
-        startTime: Instant,
-        endTime: Instant,
-        period: Duration
-): ReceiveChannel<CandleItem> = produce<CandleItem> {
-    val candleBuilder = CandleBuilder(startTime, period)
-    takeWhile { it.value.time < endTime }.consumeEach {
-        val candle = candleBuilder.addAndBuild(it)
-        if (candle != null) {
-            send(candle)
-        }
-    }
-    val lastCandle = candleBuilder.buildLast()
-    if (lastCandle != null) {
-        send(lastCandle)
-    }
-}
-
-private fun ReceiveChannel<CandleItem>.candlesWithoutTrades(
-        startTime: Instant,
-        endTime: Instant,
-        period: Duration
-): ReceiveChannel<CandleItem> = produce {
-    var last: CandleItem? = null
-    val endIndex = periodNum(startTime, period, endTime)
-
-    consumeEach {
-        val startIndex = if (last != null) last!!.index.num + 1 else 0
-        require(it.index.num >= startIndex)
-
-        for (i in startIndex until it.index.num) {
-            send(CandleItem(NumIdIndex(i, it.index.id), it.value))
-        }
-
-        send(it)
-
-        last = it
-    }
-
-    last?.let {
-        for (i in it.index.num until endIndex) {
-            send(CandleItem(NumIdIndex(i, it.index.id), it.value))
         }
     }
 }
