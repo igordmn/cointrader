1d82eb7bb594f540c72b39f960d38f536a0f7ee7
Igor Demin <igordmn@gmail.com>
2018-03-06 12:51:59 +0300

    -

--- app/src/main/kotlin/main/test/Config.kt
+++ app/src/main/kotlin/main/test/Config.kt
@@ -7,18 +7,16 @@ import java.time.*
 data class Config(
         val mainCoin: String = "BTC",
         val altCoins: List<String> = listOf(
-                "USDT", "ETH", "TRX", "XRP", "LTC", "ETC", "ICX", "VEN", "NEO", "ADA", "XLM", "HSR", "EOS",
-                "BCH", "LSK", "POE", "PPT", "APPC", "MTL", "IOTA", "WTC", "ENG", "ZRX", "XVG", "OMG", "BRD",
-                "ADX", "KNC", "DGD", "QTUM", "ZEC", "GXS", "XMR", "CND", "LEND", "STRAT", "VIBE", "BTG", "ELF",
-                "FUN", "BTS", "AION", "DASH", "GAS"
+                "USDT", "ETH", "NANO", "TRX", "ETC", "LTC", "XRP", "DGD", "VEN", "NEO", "ICX", "ADA", "BCPT", "XVG", "XLM", "EOS", "HSR", "LSK", "BCH",
+                "MTL", "NEBL", "OMG", "XMR", "GVT", "WTC", "IOTA", "INS", "IOST", "ARN", "BRD", "STRAT", "GXS", "OST"
         ),
         val initialCoins: Map<String, BigDecimal> = mapOf(
                 "BTC" to BigDecimal("1.00")
         ),
         val period: Duration = Duration.ofMinutes(5),
-        val historyCount: Int = 160,
-        val fee: BigDecimal = BigDecimal("0.0014"),
-        val learningRate: BigDecimal = BigDecimal("0.00028"),
+        val historyCount: Int = 320,
+        val fee: BigDecimal = BigDecimal("0.0018"),
+        val learningRate: BigDecimal = BigDecimal("0.00056"),
         val indicators: AdviseIndicators = AdviseIndicators.CLOSE_HIGH_LOW,
         val backTestStartTime: Instant = Instant.now() - Duration.ofDays(5),
 
