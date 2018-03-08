bbfc6094f12e21eb3bbaf2502f6a0eb7b5ff65ae
Igor Demin <igordmn@gmail.com>
2018-03-08 20:40:03 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/app/bot/trade/Trade.kt
+++ app/src/main/kotlin/com/dmi/cointrader/app/bot/trade/Trade.kt
@@ -11,35 +11,12 @@ import java.awt.Toolkit
 import java.time.Duration
 import java.time.Instant
 
-interface Fun<in IN, out OUT> {
-    suspend operator fun invoke(value: IN): OUT
-}
+typealias Command<IN> = suspend (IN) -> Unit
+typealias Action = suspend () -> Unit
+typealias Property<OUT> = suspend () -> OUT
+typealias Value<IN, OUT> = suspend (IN) -> OUT
 
-interface Command<in IN> : Fun<IN, Unit> {
-    override suspend operator fun invoke(value: IN)
-}
-
-interface Action : Command<Unit> {
-    override suspend operator fun invoke(value: Unit) = invoke()
-    suspend operator fun invoke()
-}
-
-interface Value<out OUT> : Fun<Unit, OUT> {
-    override suspend operator fun invoke(value: Unit): OUT = invoke()
-    suspend operator fun invoke(): OUT
-}
-
-fun action(invoke: suspend () -> Unit) = object : Action {
-    suspend override fun invoke() = invoke()
-}
-
-fun <T> command(invoke: suspend (T) -> Unit) = object : Command<T> {
-    suspend override fun invoke(value: T) = invoke(value)
-}
-
-fun <T> Command<T>.curry(value: T) = action { invoke(value) }
-
-fun performRealTrades(periods: Periods, getCurrentTime: Value<Instant>, trade: Action, log: Logger) = action {
+suspend fun performRealTrades(periods: Periods, getCurrentTime: Property<Instant>, trade: Action, log: Logger) {
     realTradeSignals(periods, getCurrentTime).consumeEach {
         try {
             trade()
@@ -50,13 +27,13 @@ fun performRealTrades(periods: Periods, getCurrentTime: Value<Instant>, trade: A
     }
 }
 
-fun performTestTrades(periods: Periods, timeRange: InstantRange, trade: Action) = action {
+suspend fun performTestTrades(periods: Periods, timeRange: InstantRange, trade: Action) {
     testTradeSignals(periods, timeRange).consumeEach {
         trade()
     }
 }
 
-fun realTradeSignals(periods: Periods, getCurrentTime: Value<Instant>): ReceiveChannel<Unit> = produce {
+fun realTradeSignals(periods: Periods, getCurrentTime: Property<Instant>): ReceiveChannel<Unit> = produce {
     while (isActive) {
         val currentTime = getCurrentTime().apply {
             require(this > periods.startTime)
@@ -82,9 +59,9 @@ private fun timeForNextTrade(periods: Periods, currentTime: Instant): Duration {
 }
 
 fun <PORTFOLIO> trade(
-        portfolio: Value<PORTFOLIO>,
+        portfolio: Property<PORTFOLIO>,
         rebalance: Command<PORTFOLIO>,
-        bestPortfolio: Fun<PORTFOLIO, PORTFOLIO>
+        bestPortfolio: Value<PORTFOLIO, PORTFOLIO>
 ) = action {
     rebalance(bestPortfolio(portfolio()))
 }
