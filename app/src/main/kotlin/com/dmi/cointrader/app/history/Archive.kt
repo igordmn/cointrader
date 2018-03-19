3fbee88c425d9c4afb097927b2bac6b33274a972
Igor Demin <igordmn@gmail.com>
2018-03-19 19:00:37 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/app/history/Archive.kt
+++ app/src/main/kotlin/com/dmi/cointrader/app/history/Archive.kt
@@ -1,14 +1,15 @@
 package com.dmi.cointrader.app.history
 
+import com.dmi.cointrader.app.binance.Asset
 import com.dmi.cointrader.app.binance.BinanceExchange
 import com.dmi.cointrader.app.candle.Period
 import com.dmi.cointrader.app.trade.TradeConfig
 import com.dmi.util.io.SyncFileList
 import com.dmi.util.io.syncFileList
 import java.nio.file.Files.createDirectories
-import java.nio.file.Paths
 import java.time.Instant
 import com.dmi.util.io.SyncFileList.EmptyLog
+import java.nio.file.FileSystem
 
 typealias History = List<Moment>
 
@@ -17,8 +18,28 @@ interface Archive {
     suspend fun sync(currentTime: Instant)
 }
 
-suspend fun archive(config: TradeConfig, exchange: BinanceExchange, currentTime: Instant): Archive {
-    val cacheDir = Paths.get("data/cache/binance")
+suspend fun archive(
+        fileSystem: FileSystem,
+        config: TradeConfig,
+        exchange: BinanceExchange,
+        currentTime: Instant
+): Archive {
+    fun tradeAppendLog(asset: String) = object : SyncFileList.Log<Trade> {
+        override fun itemsAppended(items: List<Trade>, indices: LongRange) {
+            val lastTradeTime = items.last().time
+            println("Trade cached: $asset $lastTradeTime")
+        }
+    }
+
+    fun momentAppendLog() = object : SyncFileList.Log<Moment> {
+        override fun itemsAppended(items: List<Moment>, indices: LongRange) {
+            val endPeriod = Period(indices.last).next()
+            val time = config.periods.startOf(endPeriod)
+            println("Moment cached: $time")
+        }
+    }
+
+    val cacheDir = fileSystem.getPath("data/cache/binance")
     val tradesDir = cacheDir.resolve("trades")
     val momentsFile = cacheDir.resolve("moments")
     createDirectories(cacheDir)
@@ -31,7 +52,6 @@ suspend fun archive(config: TradeConfig, exchange: BinanceExchange, currentTime:
         val market = normalMarket ?: reversedMarket!!
         val isReversed = normalMarket == null
         val name = if (isReversed) "$asset$mainAsset" else "$mainAsset$asset"
-
         val list = syncFileList(
                 tradesDir.resolve(name),
                 BinanceTradeConfig.serializer(),
@@ -39,18 +59,19 @@ suspend fun archive(config: TradeConfig, exchange: BinanceExchange, currentTime:
                 TradeFixedSerializer,
                 BinanceTradeConfig(name)
         )
-        list.sync(
-                BinanceTrades(market, currentTime),
-                tradeAppendLog(asset)
-        )
-        TradesArchive(market, list, isReversed)
+        object {
+            val asset = asset
+            val isReversed = isReversed
+            val market = market
+            val list = list
+        }
     }
 
     val tradeLists = trades.map {
         if (it.isReversed) {
-            it.list.map(Trade::reverse)
-        } else {
             it.list
+        } else {
+            it.list.map(Trade::reverse)
         }
     }
     val momentsList = syncFileList(
@@ -60,14 +81,21 @@ suspend fun archive(config: TradeConfig, exchange: BinanceExchange, currentTime:
             MomentFixedSerializer(config.assets.alts.size),
             MomentsConfig(config.periods, config.assets.alts)
     )
+
+    trades.forEach {
+        it.list.sync(
+                BinanceTrades(it.market, currentTime),
+                tradeAppendLog(it.asset)
+        )
+    }
     momentsList.sync(
             TradeMoments(config.periods, tradeLists, currentTime),
-            EmptyLog()
+            momentAppendLog()
     )
 
     return object : Archive {
         suspend override fun historyAt(period: Period, size: Int): History {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            momentsList
         }
 
         suspend override fun sync(currentTime: Instant) {
@@ -82,14 +110,5 @@ suspend fun archive(config: TradeConfig, exchange: BinanceExchange, currentTime:
                     EmptyLog()
             )
         }
-    }
-}
-
-private class TradesArchive(val market: BinanceExchange.Market, val list: SyncFileList<BinanceTradeState, Trade>, val isReversed: Boolean)
-
-private fun tradeAppendLog(asset: String) = object: SyncFileList.Log<Trade> {
-    override fun itemsAppended(items: List<Trade>, indices: LongRange) {
-        val lastTradeTime = items.last().time
-        println("$asset $lastTradeTime")
     }
 }
