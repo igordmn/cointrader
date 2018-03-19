cf1d079648ead743d6cbb212e0dcd7103ee054e5
Igor Demin <igordmn@gmail.com>
2018-03-19 12:48:34 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/app/test/TestExchange.kt
+++ app/src/main/kotlin/com/dmi/cointrader/app/test/TestExchange.kt
@@ -6,22 +6,38 @@ import com.dmi.cointrader.app.broker.Broker
 import com.dmi.cointrader.app.performtrade.TradeAssets
 import java.math.BigDecimal
 
-class TestExchange(private val assets: TradeAssets, private val fee: Double) {
-    private val portfolio = mapOf(assets.main to BigDecimal.ONE) + assets.alts.associate { it to BigDecimal.ZERO }
+class TestExchange(private val assets: TradeAssets, private val fee: BigDecimal) {
+    private val portfolio = HashMap(mapOf(assets.main to BigDecimal.ONE) + assets.alts.associate { it to BigDecimal.ZERO })
 
-    fun portfolio(): Portfolio = portfolio
+    fun portfolio(): Portfolio = HashMap(portfolio)
 
-    fun broker(baseAsset: Asset, quoteAsset: Asset): Broker? {
+    fun broker(baseAsset: Asset, quoteAsset: Asset, quotePrice: BigDecimal): Broker? {
         return if (baseAsset == assets.main && quoteAsset in assets.alts) {
             object: Broker {
                 override val limits = Broker.Limits(BigDecimal.ZERO, BigDecimal.ZERO)
 
-                suspend override fun buy(baseAmount: BigDecimal): Broker.OrderResult {
-
+                suspend override fun buy(baseAmount: BigDecimal): Broker.OrderResult = synchronized(portfolio) {
+                    val currentBaseAmount = portfolio[baseAsset]!!
+                    val currentQuoteAmount = portfolio[quoteAsset]!!
+                    val quoteAmount = baseAmount * quotePrice
+                    if (quoteAmount > currentQuoteAmount) {
+                        throw Broker.OrderError.InsufficientBalance
+                    }
+                    portfolio[baseAsset] = currentBaseAmount + baseAmount * (BigDecimal.ONE - fee)
+                    portfolio[quoteAsset] = currentQuoteAmount - quoteAmount
+                    return Broker.OrderResult(1.0)
                 }
 
-                suspend override fun sell(baseAmount: BigDecimal): Broker.OrderResult {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                suspend override fun sell(baseAmount: BigDecimal): Broker.OrderResult = synchronized(portfolio) {
+                    val currentBaseAmount = portfolio[baseAsset]!!
+                    val currentQuoteAmount = portfolio[quoteAsset]!!
+                    val quoteAmount = baseAmount * quotePrice
+                    if (baseAmount > currentBaseAmount) {
+                        throw Broker.OrderError.InsufficientBalance
+                    }
+                    portfolio[baseAsset] = currentBaseAmount - baseAmount
+                    portfolio[quoteAsset] = currentQuoteAmount + quoteAmount * (BigDecimal.ONE - fee)
+                    return Broker.OrderResult(1.0)
                 }
             }
         } else {
