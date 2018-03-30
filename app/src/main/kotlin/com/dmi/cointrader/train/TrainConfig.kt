b9964703b0e83be269f01fccd76d675168174730
Igor Demin <igordmn@gmail.com>
2018-03-30 10:54:54 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/train/TrainConfig.kt
+++ app/src/main/kotlin/com/dmi/cointrader/train/TrainConfig.kt
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME
 
 data class TrainConfig(
         val fee: Double = 0.0010,
-        val range: InstantRange = ISO_LOCAL_DATE_TIME.parseInstantRange("2017-07-01T00:00:00", "2018-03-18T20:20:00", zoneOffset("+3")),
+        val range: InstantRange = ISO_LOCAL_DATE_TIME.parseInstantRange("2017-07-01T00:00:00", "2018-03-19T20:20:00", zoneOffset("+3")),
 
         val testDays: Double = 14.0,         //  days for test every log step, train includes these days
         val validationDays: Double = 14.0,   //  days for check overfitting, train doesn't include these days
