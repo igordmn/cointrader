173bb8943f00e723c48df9cb9493605df9f485c1
Igor Demin <igordmn@gmail.com>
2018-04-02 15:49:22 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/train/Train.kt
+++ app/src/main/kotlin/com/dmi/cointrader/train/Train.kt
@@ -13,6 +13,7 @@ import com.dmi.util.io.appendLine
 import com.dmi.util.io.appendText
 import com.dmi.util.io.deleteRecursively
 import com.dmi.util.io.resourceContext
+import com.dmi.util.math.geoMean
 import kotlinx.coroutines.experimental.channels.consumeEachIndexed
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -39,12 +40,6 @@ suspend fun train() = resourceContext {
     val (trainPeriods, testPeriods, validationPeriods) = periods.prepareForTrain(tradeConfig, trainConfig)
     val batches = trainBatches(archive, trainPeriods, tradeConfig, trainConfig)
 
-    fun train(batch: TrainBatch): Double {
-        val (newPortions, geometricMeanProfit) = trainer.train(batch.currentPortfolio, batch.history)
-        batch.setCurrentPortfolio(newPortions)
-        return geometricMeanProfit
-    }
-
     fun saveNet(result: TrainResult) {
         net.save(netFolder(result.step))
         resultsFile.appendLine(result.toString())
@@ -55,7 +50,8 @@ suspend fun train() = resourceContext {
 
     var trainProfits = ArrayList<Double>(trainConfig.logSteps)
     batches.channel().consumeEachIndexed { (i, it) ->
-        val trainProfit = train(it)
+        val (newPortions, trainProfit) = trainer.train(it.currentPortfolio, it.history)
+        it.setCurrentPortfolio(newPortions)
         trainProfits.add(trainProfit)
         if (i % trainConfig.logSteps == 0) {
             saveNet(trainResult(
@@ -63,8 +59,8 @@ suspend fun train() = resourceContext {
                     tradePeriods = tradeConfig.tradePeriods.size,
                     step = i,
                     trainProfits = trainProfits,
-                    testCapitals = performTestTradesFast(testPeriods, tradeConfig, net, archive, trainConfig.fee),
-                    validationCapitals = performTestTradesFast(validationPeriods, tradeConfig, net, archive, trainConfig.fee)
+                    testProfits = performTestTradesFast(validationPeriods, tradeConfig, net, archive, trainConfig.fee).profits(),
+                    validationProfits = performTestTradesFast2(validationPeriods, tradeConfig, net, archive, trainConfig.fee).profits()
             ))
             trainProfits = ArrayList(trainConfig.logSteps)
         }
