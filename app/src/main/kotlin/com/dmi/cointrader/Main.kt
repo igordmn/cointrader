6345d8c5dc82c53f8de9f58584169ac64e2e7612
Igor Demin <igordmn@gmail.com>
2018-04-04 10:37:01 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/Main.kt
+++ app/src/main/kotlin/com/dmi/cointrader/Main.kt
@@ -1,10 +1,9 @@
 package com.dmi.cointrader
 
 import com.dmi.cointrader.archive.downloadArchive
-import com.dmi.cointrader.dl4j.simpleToy
+import com.dmi.cointrader.dl4j.dl4jtest
 import com.dmi.cointrader.info.printTopCoins
 import com.dmi.cointrader.trade.askAndPerformRealTrades
-import com.dmi.cointrader.trade.performRealTrades
 import com.dmi.cointrader.train.train
 import kotlinx.coroutines.experimental.runBlocking
 
@@ -15,7 +14,7 @@ fun main(args: Array<String>) {
             "train" -> train()
             "realtrades" -> askAndPerformRealTrades()
             "topcoins" -> printTopCoins()
-            "dl4jtest" -> simpleToy()
+            "dl4jtest" -> dl4jtest()
         }
     }
 }
