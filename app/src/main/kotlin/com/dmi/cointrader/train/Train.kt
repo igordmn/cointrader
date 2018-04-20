31e07989c6e69cc09f95d624344030ffecdfbbd6
Igor Demin <igordmn@gmail.com>
2018-04-20 19:54:44 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/train/Train.kt
+++ app/src/main/kotlin/com/dmi/cointrader/train/Train.kt
@@ -84,7 +84,7 @@ suspend fun train() {
                             trainProfits = trainProfits,
                             testCapitals = listOf(
                                     performTestTradesAllInFast(validationPeriods, tradeConfig, net, archive, trainConfig.fee),
-                                    performTestTradesAllInFast(testPeriods, tradeConfig, net, archive, trainConfig.fee)
+                                    performTestTradesPartialFast(validationPeriods, tradeConfig, net, archive, trainConfig.fee)
 //                                    performTestTradesPartialFast(testPeriods, tradeConfig, net, archive, trainConfig.fee)
 //                                    performTestTradesPartialFast(validationPeriods, tradeConfig, net, archive, trainConfig.fee)
                             )
