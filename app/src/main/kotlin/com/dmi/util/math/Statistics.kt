3f1a9b5938a83f90327545a64874049ec8e76526
Igor Demin <igordmn@gmail.com>
2018-05-29 12:00:11 +0300

    -

--- app/src/main/kotlin/com/dmi/util/math/Statistics.kt
+++ app/src/main/kotlin/com/dmi/util/math/Statistics.kt
@@ -21,19 +21,6 @@ fun List<Double>.product(): Double = reduce { acc, value -> acc * value }
 
 fun List<Double>.geoMean(): Double = pow(product(), 1.0 / size)
 
-fun List<Double>.normalGeoMean(): Double {
-    var s = 0.0
-    var ws = 0.0
-    sorted().forEachIndexed { index, it ->
-        val x = 2 * index.toDouble() / (size - 1) - 1  // From -1.0 to 1.0
-        val w = exp(-(x * x) * PI)  // Normal distribution
-        s += w * ln(it)
-        ws += w
-    }
-    return exp(s / ws)
-}
-
-
 fun List<Double>.sortAndRemoveOutliers(percent: Double): List<Double> {
     val removeCount = (size * (percent / 2)).toInt()
     return sorted().drop(removeCount).dropLast(removeCount)
