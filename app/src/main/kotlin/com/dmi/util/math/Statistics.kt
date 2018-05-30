750d95d98800bf03a321045540c16f00c40d4246
Igor Demin <igordmn@gmail.com>
2018-05-30 10:55:36 +0300

    -

--- app/src/main/kotlin/com/dmi/util/math/Statistics.kt
+++ app/src/main/kotlin/com/dmi/util/math/Statistics.kt
@@ -22,11 +22,6 @@ fun List<Double>.product(): Double = reduce { acc, value -> acc * value }
 
 fun List<Double>.geoMean(): Double = pow(product(), 1.0 / size)
 
-fun List<Double>.sortAndRemoveOutliers(percent: Double): List<Double> {
-    val removeCount = (size * (percent / 2)).toInt()
-    return sorted().drop(removeCount).dropLast(removeCount)
-}
-
 fun List<Double>.limitOutliers(percent: Double): List<Double> {
     val removeCount = (size * (percent / 2)).toInt()
     val sorted = sorted().drop(removeCount).dropLast(removeCount)
