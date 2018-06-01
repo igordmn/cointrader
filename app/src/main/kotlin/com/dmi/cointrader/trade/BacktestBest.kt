c22d26c809568422c9e1861856e3a186ccaaac32
Igor Demin <igordmn@gmail.com>
2018-06-01 11:21:10 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/trade/BacktestBest.kt
+++ app/src/main/kotlin/com/dmi/cointrader/trade/BacktestBest.kt
@@ -13,15 +13,13 @@ import com.dmi.cointrader.neural.jep
 import com.dmi.cointrader.neural.trainedNetwork
 import com.dmi.cointrader.savedTradeConfig
 import com.dmi.cointrader.test.TestExchange
-import com.dmi.util.io.appendText
-import com.dmi.util.io.appendToFileName
-import com.dmi.util.io.readBytes
-import com.dmi.util.io.resourceContext
+import com.dmi.util.io.*
 import com.sun.javafx.application.PlatformImpl
 import kotlinx.serialization.cbor.CBOR
 import kotlinx.serialization.cbor.CBOR.Companion.load
 import java.awt.Desktop
 import java.nio.file.Files
+import java.nio.file.Files.createDirectories
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -43,27 +41,32 @@ suspend fun backtestBest(days: Double) = resourceContext {
             .tradePeriods(config.tradePeriods.size)
 
     val path = Paths.get("data/resultsBest")
-    val logPath = path.resolve("backtest.log")
+    val backtestPath = path.resolve("backtest")
+    val logPath = backtestPath.resolve("results.log")
+
+    backtestPath.deleteRecursively()
+    createDirectories(backtestPath)
+
+    val jep = jep()
 
     val f = object {
-        suspend fun backtest(netDir: Path) = resourceContext {
-            val jep = jep().use()
+        suspend fun backtest(num: String, netDir: Path) = resourceContext {
             val network = NeuralNetwork.load(jep, netDir, gpuMemoryFraction = 0.2).use()
             val testExchange = TestExchange(config.assets, trainConfig.fee.toBigDecimal())
             val results = performTestTrades(periods, config, network, archive, testExchange)
             val summary = tradeSummary(config.periodSpace, config.tradePeriods.size, results.map { it.totalCapital }, emptyList())
 
-            val file = netDir.appendToFileName(".png")
+            val file = backtestPath.resolve("$num.png")
             PlatformImpl.startup({})
             saveLogChart(summary.chartData, file)
-            logPath.appendText("$netDir $summary")
+            logPath.appendLine("$num $summary")
         }
     }
 
     Files.newDirectoryStream(path.resolve("charts1")).use { chartDir ->
         chartDir.forEach {
             val num = it.toFile().nameWithoutExtension
-            f.backtest(path.resolve("net$num"))
+            f.backtest(num, path.resolve("net$num"))
         }
     }
 }
