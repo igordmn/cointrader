71f30fbaa6e0fb050fa753c57c76b2edf181b67c
Igor Demin <igordmn@gmail.com>
2018-06-04 19:57:35 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/Configs.kt
+++ app/src/main/kotlin/com/dmi/cointrader/Configs.kt
@@ -52,7 +52,7 @@ data class TradeConfig(
 )
 
 data class TrainConfig(
-        val range: InstantRange = ISO_LOCAL_DATE_TIME.parseInstantRange("2017-10-01T00:00:00", "2018-06-04T18:40:00", zoneOffset("+3")),
+        val range: InstantRange = ISO_LOCAL_DATE_TIME.parseInstantRange("2017-10-01T00:00:00", "2018-06-04T19:55:00", zoneOffset("+3")),
         val testDays: Double = 50.0,
         val validationDays: Double = 0.0,
 
