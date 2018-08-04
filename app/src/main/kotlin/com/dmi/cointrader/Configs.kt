e91e7a7a9216c61cebf3bd48434eb6616778fc83
Igor Demin <igordmn@gmail.com>
2018-08-05 01:21:21 +0300

    -

--- app/src/main/kotlin/com/dmi/cointrader/Configs.kt
+++ app/src/main/kotlin/com/dmi/cointrader/Configs.kt
@@ -51,7 +51,7 @@ data class TradeConfig(
 )
 
 data class TrainConfig(
-        val range: InstantRange = ISO_LOCAL_DATE_TIME.parseInstantRange("2017-07-01T00:00:00", "2018-07-28T09:46:10", zoneOffset("+3")),
+        val range: InstantRange = ISO_LOCAL_DATE_TIME.parseInstantRange("2017-07-01T00:00:00", "2018-08-05T01:22:00", zoneOffset("+3")),
         val testDays: Double = 40.0,
         val trainDaysExclude: Double = 5.0,
 
@@ -65,7 +65,7 @@ data class TrainConfig(
         val breakSteps: Int = 10000,
         val breakProfit: Double = 1.010,
 
-        val fee: Double = 0.0015,
+        val fee: Double = 0.0016,
         val batchSize: Int = 90,
         val tradePeriodGeometricBias: Double = 2e-4
 )
