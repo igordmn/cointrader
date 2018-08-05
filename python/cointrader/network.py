5ae6c606c4c6b0ee85f8bcf04d2b24cc5a5efd41
Igor Demin <igordmn@gmail.com>
2018-08-05 13:49:20 +0300

    -

--- python/cointrader/network.py
+++ python/cointrader/network.py
@@ -63,7 +63,7 @@ def build_best_portfolio(
 
     net = eiie_dense(
         net,
-        filter_number=14,
+        filter_number=16,
         activation_function='relu6',
         regularizer="L2",
         weight_decay=5e-9,
